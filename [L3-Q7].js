b330d48ac76bae912feafb00b731ee4c85f68ac1
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-09-03 13:25:43 -0300

    Create [L3-Q7].js

---
+++ [L3-Q7].js
@@ -0,0 +1,10 @@
+var c = parseInt(prompt("Digite: "))
+var r = 3
+if (c < 10000) {
+    while (c < 10000) {
+        alert(`${c} `)
+        c = c*r
+    } if (c > 10000) {
+    alert(`${c} `)
+        }
+}    
