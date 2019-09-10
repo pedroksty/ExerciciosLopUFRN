55b42aa3cbed5ed3b849a3db90c22ea27ba30413
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-09-10 17:31:16 -0300

    Rename [L2-Q7].js to [L2-Q6].js

---
+++ [L2-Q6].js
@@ -0,0 +1,8 @@
+var a = parseFloat(prompt("Digite:"))
+var b = parseFloat(prompt("Digite:"))
+var c = parseFloat(prompt("Digite:"))
+if (c>=a && c<=b) {
+    alert(`Pertence`)
+} else {
+    alert(`Nao pertence`)
+}
