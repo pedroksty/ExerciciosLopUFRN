2473ef89888fca09bc30d3b9e902ae84aff332ed
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-09-10 17:28:19 -0300

    Create [L2-Q4].js

---
+++ [L2-Q4].js
@@ -0,0 +1,41 @@
+var dia = parseInt(prompt("Digite o dia:"))
+var mes = parseInt(prompt("Digite o mês:"))
+var ano = parseInt(prompt("Digite o ano:"))
+switch(mes) {
+        case 1:
+            alert(`${dia} de Janeiro de ${ano}`)
+            break
+        case 2:
+            alert(`${dia} de Fevereiro de ${ano}`)
+            break 
+        case 3: 
+            alert(`${dia} de Março de ${ano}`)
+            break
+        case 4:
+            alert(`${dia} de Abril de ${ano}`)
+            break
+        case 5:
+            alert(`${dia} de Maio de ${ano}`)
+            break
+        case 6:
+            alert(`${dia} de Junho de ${ano}`)
+            break
+        case 7:
+            alert(`${dia} de Julho de ${ano}`)
+            break
+        case 8:
+            alert(`${dia} de Agosto de ${ano}`)
+            break
+        case 9:
+            alert(`${dia} de Setembro de ${ano}`)
+            break
+        case 10: 
+            alert(`${dia} de Outubro de ${ano}`)
+            break
+        case 11: 
+            alert(`${dia} de Novembro de ${ano}`)
+            break
+        case 12:
+            alert(`${dia} de Dezembro de ${ano}`)
+            break
+}
