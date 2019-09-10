8256072cfb1f337cd52d285dc7f955dc80da9d4f
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-09-10 16:58:49 -0300

    Create [L2-Q7].js

---
+++ [L2-Q7].js
@@ -0,0 +1,10 @@
+var x = parseInt(prompt("Digite"))
+var d3 = x%3
+var d4 = x%4
+var d5 = x%5
+if (d3==0 && d4==0 && d5==0) {
+    alert(`Eh divisivel`)
+} else {
+    alert(`Nao eh divisivel`)
+    }
+        
