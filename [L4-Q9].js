12e47a63142b4476b92d63147030d2dafc851c1b
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-09-13 13:39:19 -0300

    Create [L4-Q9].js

---
+++ [L4-Q9].js
@@ -0,0 +1,15 @@
+var c = 0
+var x1 = 1
+var x2 = 10
+var x3 = 6
+var x4 = -1
+var x5 = 10
+while (c < 10) {
+    alert(`${x1} ${x2} ${x3} ${x4} ${x5} \n`)
+    x1 += 1
+    x2 += 10
+    x3 += 2
+    x4 -= 2
+    x5 -= 1
+    c += 1
+}
