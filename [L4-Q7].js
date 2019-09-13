4a417f138da0bf4aefee7b4312c760bcb3ec8024
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-09-13 13:37:50 -0300

    Create [L4-Q7].js

---
+++ [L4-Q7].js
@@ -0,0 +1,13 @@
+var u = parseInt(prompt("Digite: "))
+var c = 0
+var s = 1
+var x = 2
+if (s == 1) {
+    alert(`${s} `)
+    c += 1
+    }
+while (c < u) {    
+    alert(`${s}/${x} `)
+    x = x + x
+    c += 1
+}   
