1f553e3eeb8561cff2afaa387da07806b4a79162
Pedrovisck <52813792+pedroksty@users.noreply.github.com>
2019-10-09 17:49:26 -0300

    Create [L4-Q13].js

---
+++ [L4-Q13].js
@@ -0,0 +1,16 @@
+var a = 22
+var b = 53
+var c = 0
+while(a <= 53) {
+    if(a <= 30) {
+        alert(`${a} `)
+        a = a + 3
+    } else if (a > 30 && a < 40) {
+        alert(`${a} `)
+        a = a + 4
+    } else if (a >= 40) {
+        alert(`${a} `)
+        a = a + 5
+    }
+}
+// alert(`a varial a vale: ${a} \n a varial b vale: ${b } \n `)
